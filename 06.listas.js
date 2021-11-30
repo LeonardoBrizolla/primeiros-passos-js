2f50df58b7b18506ff79c383799b6a97fa5de6c5
Leonardo Brizolla <leonardobrizollars@gmail.com>
2021-11-30 15:21:35 -0300

    Criando Listas

---
+++ 06.listas.js
@@ -0,0 +1,11 @@
+console.log('Trabalhando com listas - Vetores | Arrays');
+
+const listaDeCidades = new Array(
+  `Santa Catarina`,
+  `Rio Grande do Sul`,
+  `Curitiba`
+);
+
+listaDeCidades.push(`São Paulo`);
+
+console.log(listaDeCidades);
