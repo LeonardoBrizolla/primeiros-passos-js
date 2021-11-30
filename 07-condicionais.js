cb37fcb756870f6626f8ad998852d6772f204d82
Leonardo Brizolla <leonardobrizollars@gmail.com>
2021-11-30 16:50:01 -0300

    Ifs encadeados

--- 07-condicionais.js
+++ 07-condicionais.js
@@ -1,7 +1,7 @@
 console.log('Condicionais');
 
 const idadeComprador = 18;
-
+const estaAcompanhada = true;
 const listaDeCidades = new Array(
   `Santa Catarina`,
   `Rio Grande do Sul`,
@@ -13,7 +13,12 @@ console.log(listaDeCidades);
 
 if (idadeComprador >= 18) {
   listaDeCidades.splice(1, 1);
-  console.log('Passagem comprada, obrigado pela preferencia :-)');
+  console.log(
+    'Comprador maior de idade. Passagem comprada, obrigado pela preferencia :-)'
+  );
+} else if (estaAcompanhada) {
+  console.log('Esta acompanhada');
+  listaDeCidades.splice(1, 1);
 } else {
   console.log('Comprador deve ser maior de idade');
 }
