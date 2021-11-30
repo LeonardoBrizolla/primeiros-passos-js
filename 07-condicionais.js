cd6369af89c70a1a514d57a8afafb68b839a838c
Leonardo Brizolla <leonardobrizollars@gmail.com>
2021-11-30 17:30:08 -0300

    Operadores logicos encadeados

--- 07-condicionais.js
+++ 07-condicionais.js
@@ -1,7 +1,8 @@
 console.log('Condicionais');
 
-const idadeComprador = 18;
+const idadeComprador = 17;
 const estaAcompanhada = true;
+const temPassagemComprada = true;
 const listaDeCidades = new Array(
   `Santa Catarina`,
   `Rio Grande do Sul`,
@@ -11,16 +12,19 @@ const listaDeCidades = new Array(
 console.log('Lista de cidades disponíveis');
 console.log(listaDeCidades);
 
-if (idadeComprador >= 18) {
+if (idadeComprador >= 18 || estaAcompanhada) {
   listaDeCidades.splice(1, 1);
-  console.log(
-    'Comprador maior de idade. Passagem comprada, obrigado pela preferencia :-)'
-  );
-} else if (estaAcompanhada) {
-  console.log('Esta acompanhada');
-  listaDeCidades.splice(1, 1);
+  console.log('Passagem comprada, obrigado pela preferencia :-)');
 } else {
   console.log('Comprador deve ser maior de idade');
+}
+
+console.log('Embarque: \n');
+
+if (idadeComprador >= 18 && temPassagemComprada) {
+  console.log('Boa viagem!');
+} else {
+  console.log('Você não pode embarcar :-(');
 }
 
 console.log(listaDeCidades);
