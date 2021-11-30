f8cc26dec0d8989a3aebfb351882371c30c1e30f
Leonardo Brizolla <leonardobrizollars@gmail.com>
2021-11-30 14:48:48 -0300

    Atribuicao de variaveis

---
+++ 05-atrinuicaoDeVariaveis.js
@@ -0,0 +1,14 @@
+console.log('Trabalhando com atribuicao de variaveis');
+
+const idade = 23;
+const primeiroNome = 'Leonardo';
+const sobrenome = 'Brizolla';
+
+console.log(primeiroNome, sobrenome);
+console.log(primeiroNome + sobrenome);
+console.log(primeiroNome + ' ' + sobrenome);
+console.log(`Meu nome é: ${primeiroNome} ${sobrenome}`);
+
+const nomeCompleto = primeiroNome + ' ' + sobrenome;
+
+console.log(nomeCompleto);
