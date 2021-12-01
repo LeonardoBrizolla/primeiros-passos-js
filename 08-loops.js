7e11c0a21534978c56de546072773cc3543eaae4
Leonardo Brizolla <leonardobrizollars@gmail.com>
2021-11-30 22:55:25 -0300

    For

--- 08-loops.js
+++ 08-loops.js
@@ -7,14 +7,14 @@ const listaDeCidades = new Array(
 );
 
 const idadeComprador = 17;
-const estaAcompanhado = false;
+const estaAcompanhado = true;
 const destino = 'Rio Grande do Sul';
 let temPassagemComprada = false;
 
 console.log('\n Destinos possiveis:');
 console.log(listaDeCidades);
 
-const podeComprar = idadeComprador >= 18 && estaAcompanhado === true;
+const podeComprar = idadeComprador >= 18 || estaAcompanhado === true;
 
 let contador = 0;
 let destinoExiste = false;
@@ -28,3 +28,17 @@ while (contador < listaDeCidades.length) {
 }
 
 console.log('Destino existe: ', destinoExiste);
+
+if (podeComprar && destinoExiste) {
+  console.log('Boa viagem!');
+} else {
+  console.log('Erro no sistema!');
+}
+
+// For
+
+// for (i = 0; i < listaDeCidades.length; i++) {
+//   if (listaDeCidades[i] === destino) {
+//     console.log('Achou a city ;)');
+//   }
+// }
